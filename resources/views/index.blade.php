9f0f37d46cb3384059473f951edfff50ca1b6d52
Matthew Lambert <matthew.lambert@strangecorp.com>
2020-12-01 21:03:05 +0000

    EP3 Make the index page responsive.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div
+            class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -245,86 +246,89 @@
                 </a>
             </div>
         </div><!-- end popular-games -->
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
                     Recently Reviewed</h2>
-                <div class="recently-reviewed container space-y-12 mt-8">
+                <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                         <div class="relative flex-none">
                             <a href="#">
                                 <img src="/ff7.jpg" alt="game cover"
-                                     class="w-48 transition duration-150 hover:opacity-75 transform hover:scale-110">
+                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                             <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
-                                 style="right:-20px; bottom: -20px">
+                                 style="right: -20px; bottom: -20px">
                                 <div class="font-semibold text-xs flex justify-center items-center h-full">
                                     80%
                                 </div>
                             </div>
                         </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
-                                Final Fantasy 7 Remake
-                            </a>
+                        <div class="ml-6 lg:ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final
+                                Fantasy VII Remake</a>
                             <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <p class="mt-6 text-gray-400">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias assumenda at consectetur
-                                cumque eaque exercitationem nihil repudiandae vitae voluptas voluptates.
+                            <p class="mt-6 text-gray-400 hidden lg:block">
+                                A spectacular re-imagining of one of the most visionary games ever, Final Fantasy VII
+                                Remake rebuilds and expands the legendary RPG for today. The first game in this project
+                                will be set in the eclectic city of Midgar and presents a fully standalone gaming
+                                experience.
                             </p>
                         </div>
-                    </div>
+                    </div> <!-- end game -->
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                         <div class="relative flex-none">
                             <a href="#">
                                 <img src="/ff7.jpg" alt="game cover"
-                                     class="w-48 transition duration-150 hover:opacity-75 transform hover:scale-110">
+                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                             <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
-                                 style="right:-20px; bottom: -20px">
+                                 style="right: -20px; bottom: -20px">
                                 <div class="font-semibold text-xs flex justify-center items-center h-full">
                                     80%
                                 </div>
                             </div>
                         </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
-                                Final Fantasy 7 Remake
-                            </a>
+                        <div class="ml-6 lg:ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final
+                                Fantasy VII Remake</a>
                             <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <p class="mt-6 text-gray-400">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias assumenda at consectetur
-                                cumque eaque exercitationem nihil repudiandae vitae voluptas voluptates.
+                            <p class="mt-6 text-gray-400 hidden lg:block">
+                                A spectacular re-imagining of one of the most visionary games ever, Final Fantasy VII
+                                Remake rebuilds and expands the legendary RPG for today. The first game in this project
+                                will be set in the eclectic city of Midgar and presents a fully standalone gaming
+                                experience.
                             </p>
                         </div>
-                    </div>
+                    </div> <!-- end game -->
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                         <div class="relative flex-none">
                             <a href="#">
                                 <img src="/ff7.jpg" alt="game cover"
-                                     class="w-48 transition duration-150 hover:opacity-75 transform hover:scale-110">
+                                     class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                             <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
-                                 style="right:-20px; bottom: -20px">
+                                 style="right: -20px; bottom: -20px">
                                 <div class="font-semibold text-xs flex justify-center items-center h-full">
                                     80%
                                 </div>
                             </div>
                         </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
-                                Final Fantasy 7 Remake
-                            </a>
+                        <div class="ml-6 lg:ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Final
+                                Fantasy VII Remake</a>
                             <div class="text-gray-400 mt-1">Playstation 4</div>
-                            <p class="mt-6 text-gray-400">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias assumenda at consectetur
-                                cumque eaque exercitationem nihil repudiandae vitae voluptas voluptates.
+                            <p class="mt-6 text-gray-400 hidden lg:block">
+                                A spectacular re-imagining of one of the most visionary games ever, Final Fantasy VII
+                                Remake rebuilds and expands the legendary RPG for today. The first game in this project
+                                will be set in the eclectic city of Midgar and presents a fully standalone gaming
+                                experience.
                             </p>
                         </div>
-                    </div>
+                    </div> <!-- end game -->
                 </div>
             </div>
-            <div class="most-anticipated w-1/4">
+            <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
                     Most Anticipated</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
@@ -391,7 +395,7 @@
                                 <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
                             </div>
                         </div>
+                    </div>
                 </div>
-            </div>
-        </div><!-- end container -->
+            </div><!-- end container -->
 @endsection
