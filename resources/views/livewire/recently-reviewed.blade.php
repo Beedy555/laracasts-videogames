335943c753f2bbcab7de0f8912140c7123f6381c
Matthew Lambert <matthew.lambert@strangecorp.com>
2021-03-23 11:43:57 +0000

    EP15 Progress Bar / Rating - Part 2.

--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -8,11 +8,8 @@
                          class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                 </a>
                 @if (isset($game['rating']))
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                    <div id="review_{{ $game['slug'] }}" class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full text-xs"
                          style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            {{ $game['rating'] }}
-                        </div>
                     </div>
                 @endif
             </div>
@@ -48,3 +45,8 @@
         @endforeach
     @endforelse
 </div>
+@push('scripts')
+    @include('_rating', [
+    'event' => 'reviewedGameWithRatingAdded',
+])
+@endpush
